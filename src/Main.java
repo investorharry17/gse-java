e007bfa23237c26db1b1cb581fc28615ac0202ea
Ameh Harrison <amehharrison2020@gmail.com>
2024-05-07 03:05:56 +0100

    before upload

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,61 @@
-
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scannerObject = new Scanner(System.in);
 
-        System.out.println("Input your name");
-        String name = scannerObject.nextLine();
-        System.out.println("input your age");
-        int age = scannerObject.nextInt();
-
-        System.out.println("Hello " + name + " you are " + age + "years old ");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Starting random password genetator...");
+        int passwordLength;
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("How many character do you want the passwod to be?");
+        passwordLength = myScanner.nextInt();
+        makeRandom(passwordLength);
 
     }
+
+    public static void makeRandom(int length) {
+        Scanner myScanner = new Scanner(System.in);
+
+        Random random = new Random();
+        String fromString = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM./<>!$%^&*()-+#";
+        String passwordString = "";
+        for (int i = 0; i < length; i++) {
+
+            int randomValue = random.nextInt(fromString.length());
+            passwordString = passwordString + fromString.charAt(randomValue);
+        }
+        System.out.println("Here is your password: " + passwordString);
+
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("._");
+        System.out.println("Do you want to genetate another password?");
+        System.out.println("(1/2) ... 1 for yes and 2 for no.");
+        int reGenerate = myScanner.nextInt();
+
+        if (reGenerate == 1) {
+
+            System.out.println("How many character do you want the passwod to be?");
+            int passwordLength = myScanner.nextInt();
+
+            makeRandom(passwordLength);
+        } else if (reGenerate == 2) {
+            System.out.println("Stopping...");
+        } else {
+
+            System.out.println("Selected option was neither 1 or 2.");
+            System.out.println("Stopping...");
+
+        }
+
+    }
+
 }
 
-// group 2
+// group 24
